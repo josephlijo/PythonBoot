25e2762b2bd46fda35b65536d7b372a43ffe2a2c
LJNotes <ljnotes@outlook.com>
2018-01-11 17:54:42 +0000

    Apply linter to format code

--- 01.Basics/01.Helloworld.py
+++ 01.Basics/01.Helloworld.py
@@ -1,7 +1,8 @@
+# Hello Python
 print 'Hello, world'
 
-def greet(): 
-	print 'Hello from Python'
-	
-greet();
-print __name__ # prints __main__ because it is executed as a program
+def greet():
+    print 'Hello from Python'
+
+greet()
+print __name__ # prints __main__ if it is executed as a program
