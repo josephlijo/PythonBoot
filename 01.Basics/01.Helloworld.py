643a306cbabf8eaca46b1339e96fa778e6b52cbe
LJNotes <ljnotes@outlook.com>
2018-03-23 15:16:12 +0000

    Add README.md and include code comments

--- 01.Basics/01.Helloworld.py
+++ 01.Basics/01.Helloworld.py
@@ -3,7 +3,7 @@ print 'Hello, world'
 
 # A function definition
 def greet():
-    """A function which prints a string"""
+    """A function which prints a string""" # These are docstrings :: https://docs.python.org/3/tutorial/controlflow.html#documentation-strings
     print 'Hello from Python'
 
 # Invoke the function
---
+++ 01.Basics/README.md
@@ -0,0 +1,4 @@
+# Basics
+
+This section holds the basic python scripts.  
+To run the script, run `python *filename*` in your favorite command line tool
